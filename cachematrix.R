651f4dfcd80f002ff1ab8ebd78301d028e5aa7e9
unknown <chinku3571@gmail.com>
2020-09-29 20:08:37 +0530

    ## matrix Inverse and cached

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# get matrix input (f$get())
+# set value of matrix inverse by cachesolve() here (cachesolve(f))
+# call inverse (f$getmean())
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        # solve() for inverse matrix
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
+        
+        
 }
+
+# Test 1
+
+#f <- makeCacheMatrix(matrix(1:4,2,2))
+
+# f$get()
+
+#[,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+
+# f$getmean()
+
+#NULL
+
+# cacheSolve(f)
+
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
+# cacheSolve(f)
+
+#getting cached data
+
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
+# f$getmean()
+
+#[,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
